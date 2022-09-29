adf690e39e2d7b27f045667e524a14b818d79355
jan-martinek <jan-martinek@users.noreply.github.com>
2022-09-29 21:43:37 +0000

    Deploying to docs from @ books-are-next/petrarca-zpevnik@7b2bb99ffcc6555697ec8355a1d925fa2aa6ee75 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -10,7 +10,7 @@ Výběr z díla, překlad a doslov Jaroslav Pokorný
 Technická redakce Jaroslava Bednářová  
 Vydala Městská knihovna v Praze  
 Mariánské nám. 1, 115 72 Praha 1  
-V MKP 2. elektronické vydání z 25. 7. 2022.
+V MKP 2. elektronické vydání z 29. 9. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-9752e4b';
+const CACHE = 'cache-7b2bb99';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
